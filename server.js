54da7be122bf33031fd0a776986b237b7b621c04
Kyle Larsen <kylelarsen.business@gmail.com>
2022-09-20 15:46:37 -0600

    deleted my original code and refactored.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const uuid = require('uuid');
 const note = require('./db/db.sjon');
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+
+// in the following code i will be setting up the port and calling the express application into in this code.
+const app = express();
+const PORT = process.env.PORT || 3000;
 
