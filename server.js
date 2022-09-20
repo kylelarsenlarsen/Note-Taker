f11fdbcbedae56235804969737345f84a1c9bc7a
Kyle Larsen <kylelarsen.business@gmail.com>
2022-09-20 16:10:28 -0600

    added another line to include the public folder.

--- server.js
+++ server.js
@@ -7,3 +7,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// setting the express application to handling our data parsing.
+app.use(express.json()); // telling express to read in json.
+app.use(express.urlencoded({extended: true})); // allows us to access the querystring library which provides a way of parsing the URL query string.
+
+// i need to allow my server.js file to access the public folder to retrieve html, css, and js from that folder.
+app.use(express.static('public'));
