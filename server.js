ba7bc1b6f5eba5e111e141ad0f53e53d7e78f5b7
Kyle Larsen <kylelarsen.business@gmail.com>
2022-09-20 16:50:22 -0600

    setting up the routes.

--- server.js
+++ server.js
@@ -12,4 +12,12 @@ app.use(express.json()); // telling express to read in json.
 app.use(express.urlencoded({extended: true})); // allows us to access the querystring library which provides a way of parsing the URL query string.
 
 // i need to allow my server.js file to access the public folder to retrieve html, css, and js from that folder.
-app.use(express.static('public'));
+app.use(express.static('public')); // app.use is used to mount the specified middleware functions at the path which it's being specified.
+
+// in the following i need to retrieve the files and direct them to the correct destination.
+app.get('/api/notes', (req, res) => { // we're essentially telling express to retrieve that delcared pathway.
+    res.json(note.slice(1)); // note.slice is mean to prevent an undefined error on the saved files column.
+});
+app.get('/', (req, res) => { // setting up the index in this route.
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
