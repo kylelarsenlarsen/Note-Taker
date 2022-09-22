82e51c95514fafe3cc990943f03f8f557062900e
Kyle Larsen <kylelarsen.business@gmail.com>
2022-09-21 18:16:01 -0600

    added heroku at the last minute. changed some variable tags.

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const note = require('./db/db.sjon');
+const noteValue = require('./db/db.json');
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 // setting the express application to handling our data parsing.
 app.use(express.json()); // telling express to read in json.
-app.use(express.urlencoded({extended: true})); // allows us to access the querystring library which provides a way of parsing the URL query string.
+app.use(express.urlencoded({ extended: true })); // allows us to access the querystring library which provides a way of parsing the URL query string.
 
 // i need to allow my server.js file to access the public folder to retrieve html, css, and js from that folder.
 app.use(express.static('public')); // app.use is used to mount the specified middleware functions at the path which it's being specified.
@@ -24,36 +24,39 @@ app.get('/', (req, res) => { // setting up the index in this route.
 app.get('/notes', (req, res) => { // this is an express route to create the /notes directory on our URL then send us to the notes.html file.
     res.sendFile(path.join(__dirname, './public/notes.html'))
 });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
 
 // in the following section i need a function to create a note and assign an id so we can delete it later.
-function newNote(body, array) {
+function newNote(body, notesArray) {
     const note = body;
-    if(!Array.isArray(array))
-    array = [];
-    if(array.length === 0) // an if statement in the case that there's nothing entered. it will disallow the note from being saved.
-    array.push(0);
-    body.id = array[0];
-    array[0]++;
-    array.push(note);
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+    if (notesArray.length === 0) // an if statement in the case that there's nothing entered. it will disallow the note from being saved.
+        notesArray.push(0);
+    body.id = notesArray[0];
+    notesArray[0]++;
+    notesArray.push(note);
 
     fs.writeFileSync(
         path.join(__dirname, './db/db.json'),
-        JSON.stringify(array, null, 2) // in the case that the note is saveable we'll put it in our db.json file. we've set our value as the array, replacer as null, and the space is set in the 3rd position.
+        JSON.stringify(notesArray, null, 2) // in the case that the note is saveable we'll put it in our db.json file. we've set our value as the array, replacer as null, and the space is set in the 3rd position.
     );
     return note;
 };
 
 // need to create a route to post the generated note.
 app.post('/api/notes', (req, res) => {
-    const scribe = newNote(req.body, noteData);
+    const scribe = newNote(req.body, noteValue);
     res.json(scribe);
 });
 
 // in the following section i need a function to delete notes.
 function removeNote(id, notesArray) {
-    for(let i = 0; i < notesArray.length; i++) {
+    for (let i = 0; i < notesArray.length; i++) {
         let script = notesArray[i];
-        if(script.id == id) {
+        if (script.id == id) {
             notesArray.splice(i, 1);
 
             fs.writeFileSync(
@@ -67,7 +70,7 @@ function removeNote(id, notesArray) {
 
 // i need a delete route method.
 app.delete('/api/notes/:id', (req, res) => {
-    deleteNote(req.params.id, noteData);
+    removeNote(req.params.id, noteValue);
     res.json(true);
 });
 
